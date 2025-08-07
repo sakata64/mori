6ba0fdabb73973fc18466bc1bc37dfa5ec87e28e
Morikawa chino <morikawachino@CMMBA.local>
2025-08-07 16:03:42 +0900

    directions APIの表示成功

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,12 +2,15 @@
 import Splide from "https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.esm.min.js";
 
 document.addEventListener('DOMContentLoaded', function(){
-  new Splide('#splide', {
-    type: 'loop',
-    autoplay: true,
-    interval: 3000,
-    pauseOnHover: true,
-    arrows: true,
-    pagination: true
-  }).mount();
+  const el = document.querySelector('#splide');
+  if (el) {
+    new Splide(el, {
+      type: 'loop',
+      autoplay: true,
+      interval: 3000,
+      pauseOnHover: true,
+      arrows: true,
+      pagination: true
+    }).mount();
+  }
 });
