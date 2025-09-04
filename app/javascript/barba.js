80992183205a3e7dffa514c279d177a5c6e68427
Morikawa chino <morikawachino@CMMBA.local>
2025-09-04 16:23:59 +0900

    画面遷移のためのファイル作成

---
+++ app/javascript/barba.js
@@ -0,0 +1,33 @@
+import barba from '@barba/core';
+
+document.addEventListener('DOMContentLoaded', () => {
+  barba.init({
+    transitions: [
+      {
+        name: 'slide-left',
+        once({ next }) {
+          next.container.style.opacity = 1;
+        },
+        leave({ current }) {
+          return new Promise(resolve => {
+            current.container.style.transform = 'translateX(0)';
+            current.container.style.transition = 'transform 0.5s ease, opacity 0.5s ease';
+            requestAnimationFrame(() => {
+              current.container.style.transform = 'translateX(-100%)';
+              current.container.style.opacity = '0';
+            });
+            setTimeout(resolve, 500);
+          });
+        },
+        enter({ next }) {
+          next.container.style.transform = 'translateX(100%)';
+          next.container.style.transition = 'transform 0.5s ease, opacity 0.5s ease';
+          next.container.style.opacity = '1';
+          requestAnimationFrame(() => {
+            next.container.style.transform = 'translateX(0)';
+          });
+        }
+      }
+    ]
+  });
+});
